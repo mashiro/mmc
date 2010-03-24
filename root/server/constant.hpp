26b8addf66bbbcd096901207c8174001c7f388cc
mashiro <y.mashiro@gmail.com>
2010-03-25 08:33:56 +0900

    文字列定数をstd::stringにした

--- root/server/constant.hpp
+++ root/server/constant.hpp
@@ -1,38 +1,40 @@
 #ifndef MMC_CONSTANT_HPP_INCLUDED
 #define MMC_CONSTANT_HPP_INCLUDED
 
+#include <string>
+
 namespace mmc { namespace constant {
 
 	// global
-	const char* const crlf    = "\r\n";
-	const char* const space   = " ";
-	const char* const noreply = "noreply";
+	const std::string crlf    = "\r\n";
+	const std::string space   = " ";
+	const std::string noreply = "noreply";
 
 	// storage
-	const char* const set     = "set";
-	const char* const add     = "add";
-	const char* const replace = "replace";
-	const char* const append  = "append";
-	const char* const prepend = "prepend";
-	const char* const cas     = "cas";
+	const std::string set     = "set";
+	const std::string add     = "add";
+	const std::string replace = "replace";
+	const std::string append  = "append";
+	const std::string prepend = "prepend";
+	const std::string cas     = "cas";
 
 	// retrieval
-	const char* const get     = "get";
-	const char* const gets    = "gets";
+	const std::string get     = "get";
+	const std::string gets    = "gets";
 
 	// other
-	const char* const version = "version";
-	const char* const quit    = "quit";
+	const std::string version = "version";
+	const std::string quit    = "quit";
 
 	// result
-	const char* const error        = "ERROR";
-	const char* const client_error = "CLIENT_ERROR";
-	const char* const server_error = "SERVER_ERROR";
-	const char* const stored       = "STORED";
-	const char* const not_stored   = "NOT_STORED";
-	const char* const exists       = "EXISTS";
-	const char* const not_found    = "NOT_FOUND";
-	const char* const end          = "END";
+	const std::string error        = "ERROR";
+	const std::string client_error = "CLIENT_ERROR";
+	const std::string server_error = "SERVER_ERROR";
+	const std::string stored       = "STORED";
+	const std::string not_stored   = "NOT_STORED";
+	const std::string exists       = "EXISTS";
+	const std::string not_found    = "NOT_FOUND";
+	const std::string end          = "END";
 
 	// message
 
